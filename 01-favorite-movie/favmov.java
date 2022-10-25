20fe1293b1da6dff86893cf6cb1cdb2bdc0a6a51
AP Java Period 6 <apjavaperiod6@ac1mwk13ajc9h.78b505.nycboe.org>
2022-10-25 13:10:26 -0400

    added mp1 work

---
+++ 01-favorite-movie/favmov.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class favmov here.
+ *
+ * @author (Henrik Wu)
+ * @version (a version number or a date)
+ */
+public class favmov
+{
+    public static void main(String[] args){
+        System.out.println("My favorite movies are the Indiana Jones series.");
+        System.out.println("They are interesting to watch because of the action");
+        System.out.println("packed adventure.");
+        System.out.println("It is about this archeologist named Indiana Jones");
+        System.out.println("and he goes around uncovering ancient artifacts.");
+    }
+}
---
+++ 02-rocketship/RocketShip.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class RocketShip here.
+ *
+ * @author (Henrik Wu)
+ * @version (a version number or a date)
+ */
+public class RocketShip{
+    public static void main(String[] args){
+        triangle();
+        box();
+        us();
+        box();
+        triangle();
+    }
+    public static void triangle(){
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+        
+                     
+    }
+    public static void box(){
+        System.out.println("+______+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+______+");
+    }
+    public static void us(){
+        System.out.println("+______+");
+        System.out.println("|United|");
+        System.out.println("|States|");
+        System.out.println("+______+");
+    }
+    }
